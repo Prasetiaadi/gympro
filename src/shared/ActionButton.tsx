9422a6888b559d603bc2df17124a4a3033ca54cc
Prasetiaadi <pras.adi.prakasa@gmail.com>
2022-12-29 14:38:49 +0700

    first commit

---
+++ src/shared/ActionButton.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import { SelectedPage } from "./types";
+
+type Props = {
+  children: React.ReactNode;
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const ActionButton = ({ children, setSelectedPage }: Props) => {
+  return (
+    <AnchorLink
+      className="rounded-sm bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:text-white shadow-[-3px_3px_rgba(0,0,0)] hover:shadow-[-6px_6px_rgba(0,0,0)] transition duration-200"
+      onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+      href={`#${SelectedPage.ContactUs}`}
+    >
+      {children}
+    </AnchorLink>
+  );
+};
+
+export default ActionButton;
